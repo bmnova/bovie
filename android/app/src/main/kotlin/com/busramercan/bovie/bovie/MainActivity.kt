8ee08cfd47061e40fb9c1360e5790e3962919e71
bmnova <bmnovainnovations@gmail.com>
2026-01-01 16:06:23 +0300

    Initial Flutter project setup
    Add base files for a new Flutter project including configuration, platform-specific directories for Android, iOS, and macOS, project metadata, and documentation. This commit establishes the project structure and essential build files for cross-platform development.

---
+++ android/app/src/main/kotlin/com/busramercan/bovie/bovie/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.busramercan.bovie.bovie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/example/bovie/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bovie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
